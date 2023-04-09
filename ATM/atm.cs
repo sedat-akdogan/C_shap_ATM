b5070691b1831ff91b19f0b5e591435973892056
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-04-09 23:26:25 +0100

    all functions added

--- ATM/atm.cs
+++ ATM/atm.cs
@@ -68,7 +68,7 @@ public class cardHolder
         {
             Console.WriteLine("How much $$ would you like deposit? ");
             double deposit = Double.Parse(Console.ReadLine());
-            currentUser.setBalance(deposit); // deposit + currentBalance
+            currentUser.setBalance(currentUser.getBalance() + deposit); 
             Console.WriteLine("It is done, new balance: " + currentUser.getBalance);
         }
         void withdraw(cardHolder currentUser)
@@ -88,6 +88,64 @@ public class cardHolder
             {
                 Console.WriteLine("Current balance: " + currentUser.getBalance());
             }
+            List<cardHolder> cardHolders = new List<cardHolder>();
+            cardHolders.Add(new cardHolder("123456", 1235, "alperen", "akdogan", 123.45));
+            cardHolders.Add(new cardHolder("123457", 1236, "azra", "akdogan", 145.45));
+            cardHolders.Add(new cardHolder("123458", 1237, "adjoa", "akdogan", 187.45));
+            cardHolders.Add(new cardHolder("123459", 1238, "esra", "akdogan", 923.45));
+            cardHolders.Add(new cardHolder("123451", 1239, "sultan", "akdogan", 423.45));
+
+            // Prompt user
+            Console.WriteLine("Welcome to SimpleATM");
+            Console.WriteLine("Type your debit card number: ");
+            String debitCardNumber = "";
+            cardHolder currentUser;
+
+            while(true)
+            {
+                try
+                {
+                    debitCardNumber = Console.ReadLine();
+                    // check against our db
+                    currentUser = cardHolders.FirstOrDefault(a => a.cardName == debitCardNumber);
+                    if (currentUser != null) { break; }
+                    else { Console.WriteLine("cardHolder not recognized. Please try again"); }
+                }
+                catch {Console.WriteLine("cardHolder not recognized. Please try again");}
+            }
+
+            Console.WriteLine("Please enter your pin: ");
+            int userPin = 0;
+             while(true)
+            {
+                try
+                {
+                    userPin = int.Parse(Console.ReadLine());
+                    // check against our db
+                    if (currentUser.getPin() == userPin) { break; }
+                    else { Console.WriteLine("incorrent Pin. Please try again"); }
+                }
+                catch {Console.WriteLine("incorrent pin. Please try again");}
+            }
+
+            System.Console.WriteLine("Welcome " + currentUser.getFirstName());
+            int option = 0;
+            do
+            {
+                printOptions();
+                try
+                {
+                    option = int.Parse(Console.ReadLine());
+                }
+                catch { }
+                if (option == 1) { deposit(currentUser); }
+                else if (option == 2) { withdraw(currentUser); }
+                else if (option == 3) { balance(currentUser); }
+                else if (option == 4) { break; }
+                else { option = 0; }
+                
+            } while (option != 4);
+            System.Console.WriteLine("Thamk you, have a wonderful day");
         }
     }
-}
+} 
